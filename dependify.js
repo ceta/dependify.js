693e9a8ededac042c56e465f63b97454e13e636d
Max Holland <mholland1337@gmail.com>
2013-05-16 10:20:49 +0100

    Added array option

--- dependify.js
+++ dependify.js
@@ -1,121 +1,145 @@
-// usage: log('inside coolFunc',this,arguments);
-// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function () {
-    log.history = log.history || []; // store logs to an array for reference
-    log.history.push(arguments);
-    if (this.console) {
-        console.log(Array.prototype.slice.call(arguments));
-    }
-};
-
-
 var dependify = {
     DependencySelector: '.dependency',
     SectionToggleAttr: 'dependency-child',
     ToggleOn: 'dependency-show',
     MatchSelector: 'dependency-match',
     init: function () {
-            var simple = function (obj) {
-                var target = $(obj).data('dependency-child');
-                var dependent = $(obj).data('dependency-show');
-                var inpVal = obj.value;
+        var simple = function (obj) {
+            var target = $(obj).data('dependency-child');
+            var dependent = $(obj).data('dependency-show');
+            var inpVal = obj.value;
 
 
-                if (isNaN(inpVal)) {
-                    inpVal = inpVal.toLowerCase();
-                }
+            if (isNaN(inpVal)) {
+                inpVal = inpVal.toLowerCase();
+            }
 
-                if (isNaN(dependent)) {
-                    dependent = dependent.toLowerCase();
-                }
+            if (isNaN(dependent)) {
+                dependent = dependent.toLowerCase();
+            }
 
-                if (inpVal == dependent) {
-                    $(target).show();
-                } else {
+            if (inpVal == dependent) {
+                $(target).show();
+            } else {
 
-                    $(target).hide();
-                }
+                $(target).hide();
+            }
 
-            };
+        };
 
-            var or = function (obj) {
-                var target = $(obj).data('dependency-child')
-                var dependent = $(obj).data('dependency-show');
-                var inpVal = obj.value;
-                var inpArr = [];
-                var flag = false;
+        var or = function (obj) {
+            var target = $(obj).data('dependency-child');
+            var dependent = $(obj).data('dependency-show');
+            var inpVal = obj.value;
+            var inpArr = [];
+            var flag = false;
 
-                if (typeof dependent === 'string' && dependent.indexOf("|") >= 0) {
-                    inpArr = dependent.split("|");
+            if (typeof dependent === 'string' && dependent.indexOf("|") >= 0) {
+                inpArr = dependent.split("|");
 
-                    for (i = 0; i < inpArr.length; i++) {
-                        if (inpVal == inpArr[i]) {
-                            flag = true;
-                        }
+                for (i = 0; i < inpArr.length; i++) {
+                    if (inpVal == inpArr[i]) {
+                        flag = true;
                     }
-
-                } else {
-                    flag = false;
                 }
 
-                if (flag) {
-                    $(target).show();
-                } else {
-                    $(target).hide();
-                }
+            } else {
+                flag = false;
+            }
+
+            if (flag) {
+                $(target).show();
+            } else {
+                $(target).hide();
+            }
+
+        };
+
+        var array = function (obj) {
+          
+          var target = obj.getAttribute('data-dependency-child');
+          var dependent = $(obj).data('dependency-show');
+          var inpVal = obj.value;
+          var inpArr = target.split("|");
+          var depArr = $(obj).data('dependency-show').split("|");
+          var flag = false;
+                    
+          if(inpArr.length === depArr.length){
+          
+            for(i = 0; i < inpArr.length; i++){
+              if(depArr[i] == inpVal){
+                target = inpArr[i]; 
+                flag = true;
+              }
+              
+              
+              
+            }
+            
+           alert(flag);
+
+           if (flag) {
+               $(target).show();
+            } else {
+              for(i=0; i< inpArr.length;i++){
+                 $(inpArr[i]).hide();
+               }
+            }
+          
+          }
+            
+          
+          
+
+        };
+
+        $(this.DependencySelector).change(function () {
+            var match = $(this).data('dependency-match');
+            switch (match) {
+            case "simple":
+                simple(this);
+                break;
+
+            case "or":
+                or(this);
+                break;
+
+            case "array":
+                array(this);
+                break;
+
+            default:
+                simple(this);
+                break;
+
+            }
+        });
+
+        $(this.DependencySelector).each(function () {
+            var match = $(this).data('dependency-match');
+
+            switch (match) {
+            case "simple":
+                simple(this);
+                break;
+
+            case "or":
+                or(this);
+                break;
+
+            case "array":
+                array(this);
+                break;
+
+            default:
+                simple(this);
+                break;
+
+            }
 
 
-            };
+        });
 
-            $(this.DependencySelector).change(function () {
-                var match = $(this).data('dependency-match');
-                switch (match) {
-                case "simple":
-                    simple(this);
-                    break;
-
-                case "or":
-                    or(this);
-                    break;
-
-                case "array":
-
-                    break;
-
-                default:
-                    simple(this);
-                    break;
-
-                }
-            });
-
-            $(this.DependencySelector).each(function () {
-                var match = $(this).data('dependency-match');
-
-                switch (match) {
-                case "simple":
-                    simple(this);
-                    break;
-
-                case "or":
-                    or(this);
-                    break;
-
-                case "array":
-
-                    break;
-
-                default:
-                    simple(this);
-                    break;
-
-                }
-
-
-            });
-
-
-    
 
 
 
